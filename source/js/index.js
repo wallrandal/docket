6e117671a8e7453a93cbb404b484b5a76d6423af
wallace <wallace.nfesistemas@gmail.com>
2019-01-03 16:51:37 -0200

    mudança no javascript para trabalhar com classe

--- source/js/index.js
+++ source/js/index.js
@@ -1,28 +1,40 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+class Index {
+  constructor () {
+    this.slideIndex = 1
+    this.watch()
   }
 
-  if (slideIndex-1 == 0 || slideIndex-1 == 1) {
-  	slides[slideIndex-1].style.display = "block";
-  	slides[slideIndex].style.display = "block";
+  watch () {
+    this.showSlides(this.slideIndex)
+
+    $(".next").on("click", function(e) {
+      this.plusSlides(1)
+    }.bind(this));
+
+    $(".prev").on("click", function(e) {
+      this.plusSlides(-1)
+    }.bind(this));
   }
 
+  plusSlides(n) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {this.slideIndex = 1} 
+    if (n < 1) {this.slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    
+    if (this.slideIndex-1 == 0 || this.slideIndex-1 == 1) {
+      slides[this.slideIndex-1].style.display = "block";
+      slides[this.slideIndex].style.display = "block";
+    }
+
+  }
 }
+
+const index = new Index()
